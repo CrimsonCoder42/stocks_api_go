601d8a76cfd5d86c241f1052ee93720af81daee6
CrimsonCoder42 <nostro37@gmail.com>
2023-09-29 08:00:34 -0400

    postman testing final

--- router/router.go
+++ router/router.go
@@ -13,5 +13,5 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/stock", middleware.CreateStock).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/stock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
-	
+	return router
 }
